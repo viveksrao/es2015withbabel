77e3f204935d644e866a1ae92b56d21dbb698d17
Vivekanand Rao <vivek.rao@teachscape.com>
2015-11-20 21:03:45 -0800

    Simple Demo Script showcasing how to use ES2015 with Babel and Gulp.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,7 +1,13 @@
 "use strict";
 
 import gulp from "gulp";
+import browserify from "browserify";
+import source from "vinyl-source-stream";
 
 gulp.task("default",() => {
-  console.log("From Gulp!");
+  return browserify("src/app.js")
+          .transform("babelify")
+          .bundle()
+          .pipe(source("bundle.js"))
+          .pipe(gulp.dest("dist"));
 });
---
+++ src/app.js
@@ -0,0 +1,5 @@
+import FlashMessage from "./flash-message";
+
+let flash = new FlashMessage("Gulp, Babel and Browserify");
+
+flash.display();
