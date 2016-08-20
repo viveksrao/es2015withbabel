5d2ffcbe1e6d667fc08e45cc0496b2571c195c0b
Vivekanand Rao <vivekanand.rao@gmail.com>
2016-08-19 20:49:24 -0700

    Added sample script.

---
+++ build/index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var double = function double(value) {
+  return value * 2;
+};
+console.log("What is 1337 * 2?");
+console.log(double(1337));
---
+++ src/index.js
@@ -0,0 +1,3 @@
+var double = (value) => value * 2;
+console.log("What is 1337 * 2?");
+console.log(double(1337));
